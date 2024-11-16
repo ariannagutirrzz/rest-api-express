9d219d3500738129c9e7bc2bba6541667319e028
Arianna Vega <avega@equaldata.com>
2024-11-16 16:04:26 -0400

    added: get functions to interact with id from params

--- index.js
+++ index.js
@@ -6,6 +6,39 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/new-route', (req, res) => {
+  res.send('This is a new route');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    products: [
+      {
+        id: 1,
+        name: 'Product 1',
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+      },
+    ],
+  });
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  // res.send(`You requested to see an item with the id: ${id}`);
+  res.send({
+    id,
+    name: `Product ${id}`,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.send(`Category: ${categoryId}, Product: ${productId}`);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
