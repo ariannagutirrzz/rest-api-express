53abf9d8a453044dfdf1467bf5ff1ead46d374c9
Arianna Vega <avega@equaldata.com>
2025-01-06 01:06:13 -0400

    feat: add dotenv for environment variable management and enhance error handling based on environment

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,12 +1,25 @@
+require('dotenv').config();
+
 function logErrors(error, req, res, next) {
-  console.error(`Method: ${req.method}, Path: ${req.originalUrl}`);
-  console.error(error);
+  const { message, stack } = error;
+  const { method, originalUrl } = req;
+
+  if (process.env.NODE_ENV === 'development') {
+    console.error(`Method: ${method}, Path: ${originalUrl}`);
+    console.error(error, stack);
+  } else {
+    console.error(message);
+  }
   next(error);
 }
 
 function errorHandler(error, req, res, next) {
-  const { message, stack } = error;
-  res.status(500).json({ message, stack });
+  if (process.env.NODE_ENV === 'development') {
+    const { message, stack } = error;
+    res.status(500).json({ message, stack });
+  } else {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
 }
 
 module.exports = { errorHandler, logErrors };
